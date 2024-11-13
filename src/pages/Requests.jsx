35b2f74dd2a2821a1b55bf34c82dad8bd051fbf2
DiegoJesusP <spartfomi@outlook.com>
2024-11-13 12:52:52 -0600

    Crear el componente RequestCard y aplicar estilos con request.card.module.css

--- src/pages/Requests.jsx
+++ src/pages/Requests.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React from 'react';
+import '../../public/css/user/home.css';
+import RequestCard from '../components/RequestCard.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Requests = () => {
-  return (
-    <>
-      <h1 className='title py-2'>Mis solicitudes</h1>
-    </>
+    const data = [
+        { id: '1', title: 'Kardex', description: 'Descripción del kardex', status: 'Solicitud en espera'},
+        { id: '2', title: 'Constancia de estudios', description: 'Descripción del certificado', status: 'Solicitud en progreso'},
+        { id: '3', title: 'Carta de recomendación', description: 'Descripción de la constancia', status: 'Solicitud terminada'},
+        { id: '4', title: 'Certificado de estudios', description: 'Descripción de la constancia', status: 'Solicitud terminada'},
+        { id: '5', title: 'Certificado de estudios', description: 'Descripción de la constancia', status: 'Solicitud terminada'},
+        { id: '5', title: 'Kardex', description: 'Descripción de la constancia', status: 'Solicitud terminada'},
+        { id: '5', title: 'Certificado de estudios', description: 'Descripción de la constancia', status: 'Solicitud terminada'},
 
-  )
-}
+    ];
 
-export default Requests
+    return (
+        <>
+            <h1 className='title py-2'>Mis solicitudes</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    {data.map((item) => (
+                        <div key={item.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mt-3 mb-1">
+                            <RequestCard
+                                title={item.title}
+                                description={item.description}
+                                status={item.status}
+                                footerColor={item.footerColor}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Requests;
